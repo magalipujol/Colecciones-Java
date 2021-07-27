3f762e5d1049bf875d4be9ce1106e9495d523707
magalipujol <magalipuuujol@gmail.com>
2021-07-27 18:02:00 -0300

    first commit

---
+++ ejemplo.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class ejemplo {
+    public static void main(String[] args) {
+        
+        ArrayList<String> listaDecosas = new ArrayList();
+
+        listaDecosas.add("cosa1");
+        listaDecosas.add("cosa2");
+        listaDecosas.add("cosa3");
+
+        System.out.println(listaDecosas);
+
+    }
+
+}
